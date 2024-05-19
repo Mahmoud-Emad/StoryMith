79c08509964d92aa635f168c1e968c3ce5780a91
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2024-05-19 18:59:14 +0300

    feat: Define Gihub pages.

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ import viteConfig from './vite.config'
 export default mergeConfig(
   viteConfig,
   defineConfig({
+    base: "/StoryMith/",
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
